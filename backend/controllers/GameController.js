b7cba46423eb6e432d746852b0a7a964be351ca6
sohel-sk <sohelsheikh9623@gmail.com>
2025-03-31 02:05:47 +0530

    updatePoints route done

--- backend/controllers/GameController.js
+++ backend/controllers/GameController.js
@@ -39,3 +39,21 @@ export const redeemReward = async (req, res) => {
         res.status(500).json({ message: error.message });
     }    
 };
+
+export const updatePoints = async (req, res) => {
+    try {
+        const { pointsToAdd } = req.body;
+        if (!pointsToAdd || pointsToAdd < 0) {
+            return res.status(400).json({ message: "Invalid pointsToAdd value" });
+        }
+        const user = await User.findById(req.user.id);
+        if (!user) {
+            return res.status(404).json({ message: "User not found in database" });
+        }
+        user.points += pointsToAdd;
+        await user.save();
+        res.status(200).json({ message: "Points updated successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- backend/routes/gamificationRoute.js
+++ backend/routes/gamificationRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getPoints, redeemReward} from "../controllers/GameController.js";
+import { getPoints, redeemReward,updatePoints} from "../controllers/GameController.js";
 import authenticateToken from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get('/points',authenticateToken, getPoints);
+router.get('/points', authenticateToken, getPoints);
+router.put('/points', authenticateToken, updatePoints);
 // router.get('/badges/:userId', getBadges);
-router.post('/redeem-reward',authenticateToken, redeemReward);
+router.put('/redeem-reward',authenticateToken, redeemReward);
 
 export default router;
